1db2e2da8c96dae8c54cebcf5897a858264c6837
Alexandr <sashanikitenko97@mail.ru>
2017-08-08 16:52:29 +0300

    DEBUG-False

--- cvsite/settings.py
+++ cvsite/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@@-!3rzkc)ni*0w72skwr3dmff)^8uwryw&lg@89t##e)c)1nr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
